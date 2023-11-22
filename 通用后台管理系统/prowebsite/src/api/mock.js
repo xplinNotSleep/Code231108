6705e359875cce0beb2011bd0dc7e2464ee7784e
xplinNotSleep <15913990805@163.com>
2023-11-22 22:24:10 +0800

    11.22晚提交传递数据

--- 通用后台管理系统/prowebsite/src/api/mock.js
+++ 通用后台管理系统/prowebsite/src/api/mock.js
@@ -1,8 +1,15 @@
 import Mock from 'mockjs'
 import homeApi from './mockServeData/home'
+import user from './mockServeData/user'
 
 //定义mock请求拦截
 Mock.mock('/api/home/getData', homeApi.getStatisticalData)
+
+//用户列表的数据(通过请求api进行获取)
+Mock.mock('api/user/add','post',user.createUser)
+Mock.mock('/api/user/edit', 'post', user.updateUser)
+Mock.mock('/api/user/del', 'post', user.deleteUser)
+Mock.mock('/api/user/getUser', user.getUserList)
 
 /*
 Mock.mock('/api/home/getData',function(){
