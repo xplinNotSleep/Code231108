efcfe964850de3844a47977a3cef1866c401732e
xplinNotSleep <15913990805@163.com>
2023-11-23 22:21:05 +0800

    11.23晚提交

--- 通用后台管理系统/prowebsite/vue.config.js
+++ 通用后台管理系统/prowebsite/vue.config.js
@@ -2,4 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false
+
+  /*配置跨域
+  devServer:{
+    proxy:{
+      '/api':{
+        target:'http://124.223.69.156:3300',
+        changeOrigin:true,
+      }
+    }
+  }
+  */
 })
+
